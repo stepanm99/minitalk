9d8f0b4cb926a502076203d6714830dba52d0a48
stepanm99 <stepan.melichar99@seznam.cz>
2023-12-05 23:31:51 +0100

    I realized that in this state it works even with unicode characters, it just sends them in more bytes than regular ascii characters, it just somehow  works, magic probably :D

--- incl/minitalk.h
+++ incl/minitalk.h
@@ -6,7 +6,7 @@
 /*   By: smelicha <smelicha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 16:21:29 by smelicha          #+#    #+#             */
-/*   Updated: 2023/12/04 23:12:31 by smelicha         ###   ########.fr       */
+/*   Updated: 2023/12/05 19:40:08 by smelicha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,38 @@
 # define _XOPEN_SOURCE 500
 # include <signal.h>
 # include <sys/types.h>
+# include <locale.h>
+# include <wchar.h>
+
+/*
+state_flag:
+	0: receiving clinet PID -> 1
+	1: receiving data size & allocating memory -> 2
+	2: 
+bit_pos: position of received bit in character
+zeros: number of zeros in the received data
+ones: number of ones in the received data
+*/
+typedef struct s_s_data
+{
+	pid_t	client_pid;
+	int		state_flag;
+	int		bit_pos;
+	int		zeros;
+	int		ones;
+}	t_s_data;
+
+/*
+
+*/
+typedef struct	s_c_data
+{
+	pid_t	server_pid;
+	int		state_flag;
+	int		zeros;
+	int		ones;
+	int		outgoing_data_size;
+}	t_c_data;
+
 
 #endif
